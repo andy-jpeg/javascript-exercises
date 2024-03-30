5bf6ea4874c067aabe76237ba30dfe2821356442
andy <156564624+andy-jpeg@users.noreply.github.com>
2024-03-29 23:05:47 -0500

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let chars = string.split("")
+    let newString = ""
 
+    for (i = (chars.length - 1); i >= 0; i--) {
+        newString += chars[i]
+    }
+
+    return newString
 };
 
 // Do not edit below this line
