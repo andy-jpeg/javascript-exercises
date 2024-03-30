dddcf5e203f32f3e2ab7b03059a8c7471b9db135
andy <156564624+andy-jpeg@users.noreply.github.com>
2024-03-29 23:27:32 -0500

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let newValue = ((value - 32) * (5/9))
+  newValue = Math.round(newValue * 10) / 10
+
+  return newValue
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let newValue = ((value * (9/5)) + 32)
+  newValue = Math.round(newValue * 10) / 10
+
+  return newValue
 };
 
 // Do not edit below this line
